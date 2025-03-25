e5d2e1c0f64b6489ffab7bd4060dd8b5bd2de3d1
Egemen Yıldırım <Eqemen1912@gmail.com>
2025-03-25 11:33:34 +0300

    create success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Header from '../components/Header'
+import styled from 'styled-components'
+
+const SuccessContainer = styled.div`
+background-color:  #CE2829; 
+  min-height: 100vh;
+  min-width:100vw; 
+  display: flex;
+  position: fixed; 
+  top: 0; 
+  left: 0;
+   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white; 
+  p{
+  font-size:56px;}
+`;
 
 function Success() {
   return (
-    <div>
+    <SuccessContainer>
       <Header />
-      
-    </div>
+      <p>TEBRİKLER!</p>
+      <p>SİPARİŞİNİZ ALINDI!</p>
+    </SuccessContainer>
   )
 }
 
